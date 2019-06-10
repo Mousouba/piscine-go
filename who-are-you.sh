554784cd5e17ec7a686290be70760660effdd1e6
Mousouba <mousouba@nan.ci>
2019-06-10 00:20:06 +0000

    Who are you?

--- who-are-you.sh
+++ who-are-you.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 array=$(curl https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json)
-echo $array | jq '.[] | select(.id==70)' | jq '.name' | cut -d'"' -f 2
+echo $array | jq '.[] | select(.id==70)' | jq '.name'
+# | cut -d'"' -f 2
 
 
